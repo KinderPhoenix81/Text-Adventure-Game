843faa2069fcad59626721304a7a632421fd0e3c
KinderPhoenix81 <allenalex642@gmail.com>
2025-11-02 13:18:20 -0600

    Added logger library folder & logging example

--- src/textAdventure/TryCatchExample.java
+++ src/textAdventure/TryCatchExample.java
@@ -1,20 +1,29 @@
 package textAdventure;
 
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TryCatchExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Try catch example
 		Scanner scanner = new Scanner(System.in);
+		Logger logger = LogManager.getLogger(Main.class);
 		
 		System.out.println("Enter a number");
 		try {
 			int num = scanner.nextInt();
+			
+			//Debug
 			System.out.println("You did it");
+			logger.info("You're number was" + num);
 		} catch (java.util.InputMismatchException ex) {
+			//Display error
 			System.out.println("That's not a number:(");
+			logger.error(ex.getStackTrace());
 		} finally {
+			//Debug
 			System.out.println("Goodbye");
 		}
 		
