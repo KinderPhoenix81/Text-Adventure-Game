cdc01536428a396be21e76c1c9ef8f13352d4fbe
KinderPhoenix81 <allenalex642@gmail.com>
2025-11-02 13:41:53 -0600

    Implemented basic logging

--- src/textAdventure/TryCatchExample.java
+++ src/textAdventure/TryCatchExample.java
@@ -6,27 +6,26 @@ import org.apache.logging.log4j.Logger;
 
 public class TryCatchExample {
 
+	final static Logger log = LogManager.getLogger("LoggingExample");
+	
 	public static void main(String[] args) {
 		//Try catch example
 		Scanner scanner = new Scanner(System.in);
-		Logger logger = LogManager.getLogger(Main.class);
-		
 		System.out.println("Enter a number");
 		try {
 			int num = scanner.nextInt();
 			
 			//Debug
 			System.out.println("You did it");
-			logger.info("You're number was" + num);
+			log.info("You're number was " + num);
 		} catch (java.util.InputMismatchException ex) {
 			//Display error
 			System.out.println("That's not a number:(");
-			logger.error(ex.getStackTrace());
+			log.error("Invalid Input", ex);
 		} finally {
 			//Debug
 			System.out.println("Goodbye");
 		}
-		
 		
 	}
 
