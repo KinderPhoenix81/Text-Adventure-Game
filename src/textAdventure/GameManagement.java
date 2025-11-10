eb3c97347a05aac6d352b943b90b7973af45011e
KinderPhoenix81 <allenalex642@gmail.com>
2025-11-09 22:23:50 -0600

    Added Rooms
    Added the three remaining garden rooms. Reformatted some room headers for visibility

--- src/textAdventure/GameManagement.java
+++ src/textAdventure/GameManagement.java
@@ -103,13 +103,11 @@ public class GameManagement {
 		ArrayList<Item> gameItems = new ArrayList<Item>();
 		ArrayList<BaseInteractable> gameInteractables = new ArrayList<BaseInteractable>();
 
-		//Create the north gardens room 
+		/*
+		 * Create the north gardens room
+		 */
 		Room northGardens = new Room();
-		
-		//Set the values of the north gardens
-		northGardens.setName("North Gardens");
-		northGardens.setDesc("North Gardens desc...");
-		
+	
 		//Create lists for the actions, items, and interactables in the room
 		ArrayList<String> ngRoomActions = new ArrayList<>(List.of("Move South", "Inspect Pedestal", "Inspect Rock Engravings", "Pick up Wheat Totem"));
 		
@@ -119,7 +117,9 @@ public class GameManagement {
 		ArrayList<BaseInteractable> ngRoomInteractables = new ArrayList<>();
 		Pedestal northGardensPedestal = new Pedestal("Pedestal", "North Pedestal desc..", "North Pedestal lore...", wheatTotem);
 		
-		//Add the lists to the room
+		//Set the values of the north gardens
+		northGardens.setName("North Gardens");
+		northGardens.setDesc("North Gardens desc...");
 		northGardens.setRoomActions(ngRoomActions);
 		northGardens.setItemList(ngRoomItems);
 		northGardens.setInteractableList(ngRoomInteractables);
@@ -129,13 +129,83 @@ public class GameManagement {
 		gameItems.add(wheatTotem);
 		gameInteractables.add(northGardensPedestal);
 		
-		//Create the east gardens room
+		/*
+		 * Create the east gardens room
+		 */
+		Room eastGardens = new Room();
 		
-		//Create the south gardens room
+		//Lists for actions, items, and interactables in this room
+		ArrayList<String> egRoomActions = new ArrayList<>(List.of("Move West", "Inspect Pedestal", "Inspect Rock Engravings", "Pick up Sword Totem"));
 		
-		//Create the west gardens room
+		ArrayList<Item> egRoomItems = new ArrayList<>();
+		Item swordTotem = new Item("Sword Totem", "Sword Totem desc...", "Sword totem lore...", "Place Totem");
 		
-		//Create the east gardens room
+		ArrayList<BaseInteractable> egRoomInteractables = new ArrayList<>();
+		Pedestal eastGardensPedestal = new Pedestal("Pedestal", "East Pedestal desc..", "East Pedestal lore...", swordTotem);
+		
+		//Values of east gardens
+		eastGardens.setName("East Gardens");
+		eastGardens.setDesc("East Gardens desc...");
+		eastGardens.setRoomActions(egRoomActions);
+		eastGardens.setItemList(egRoomItems);
+		eastGardens.setInteractableList(egRoomInteractables);
+		
+		//Add the individual items to the overall game lists
+		gameRooms.add(eastGardens);
+		gameItems.add(swordTotem);
+		gameInteractables.add(eastGardensPedestal);
+		
+		/*
+		 * Create the south gardens room
+		 */
+		Room southGardens = new Room();
+		
+		//Lists for actions, items, and interactables in this room
+		ArrayList<String> sgRoomActions = new ArrayList<>(List.of("Move North", "Inspect Pedestal", "Inspect Rock Engravings", "Pick up Fish Totem"));
+		
+		ArrayList<Item> sgRoomItems = new ArrayList<>();
+		Item fishTotem = new Item("Fish Totem", "Fish Totem desc...", "Fish totem lore...", "Place Totem");
+		
+		ArrayList<BaseInteractable> sgRoomInteractables = new ArrayList<>();
+		Pedestal southGardensPedestal = new Pedestal("Pedestal", "South Pedestal desc..", "South Pedestal lore...", fishTotem);
+		
+		//Values of east gardens
+		southGardens.setName("South Gardens");
+		southGardens.setDesc("South Gardens desc...");
+		southGardens.setRoomActions(sgRoomActions);
+		southGardens.setItemList(sgRoomItems);
+		southGardens.setInteractableList(sgRoomInteractables);
+		
+		//Add the individual items to the overall game lists
+		gameRooms.add(southGardens);
+		gameItems.add(fishTotem);
+		gameInteractables.add(southGardensPedestal);
+		
+		/*
+		 * Create the west gardens room
+		 */
+		Room westGardens = new Room();
+		
+		//Lists for actions, items, and interactables in this room
+		ArrayList<String> wgRoomActions = new ArrayList<>(List.of("Move East", "Inspect Pedestal", "Inspect Rock Engravings", "Pick up Lumber Totem"));
+		
+		ArrayList<Item> wgRoomItems = new ArrayList<>();
+		Item lumberTotem = new Item("Lumber Totem", "Lumber Totem desc...", "Lumber totem lore...", "Place Totem");
+		
+		ArrayList<BaseInteractable> wgRoomInteractables = new ArrayList<>();
+		Pedestal westGardensPedestal = new Pedestal("Pedestal", "West Pedestal desc..", "West Pedestal lore...", lumberTotem);
+		
+		//Values of east gardens
+		westGardens.setName("West Gardens");
+		westGardens.setDesc("West Gardens desc...");
+		westGardens.setRoomActions(wgRoomActions);
+		westGardens.setItemList(wgRoomItems);
+		westGardens.setInteractableList(wgRoomInteractables);
+		
+		//Add the individual items to the overall game lists
+		gameRooms.add(westGardens);
+		gameItems.add(lumberTotem);
+		gameInteractables.add(westGardensPedestal);
 		
 		//Create the crypt entrance (inside) room
 		
